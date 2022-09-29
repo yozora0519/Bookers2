ecb347a3df25e693a38dea4a08b93712919224d6
yozora0519 <ec2-user@ip-172-31-34-233.ap-northeast-1.compute.internal>
2022-09-29 11:44:21 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root "homes#top"
 
   get "/home/about" => "homes#about"
+  get 'books/index'
   resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
