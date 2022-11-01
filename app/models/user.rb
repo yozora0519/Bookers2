984707c6f80f7750d60f0dca5d28d2a89088515f
yozora0519 <ec2-user@ip-172-31-34-233.ap-northeast-1.compute.internal>
2022-11-01 08:35:07 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
